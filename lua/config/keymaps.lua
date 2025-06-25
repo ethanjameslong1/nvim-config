5175a034a42cd72def2bb0600a477dce7fcb0d20
ethanjameslong1 <el23t@fsu.edu>
2025-06-25 13:00:55 -0400

    Initial commit of Neovim configuration

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,73 @@
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil Float" })
+vim.keymap.set("n", "gl", function()
+	vim.diagnostics.open_float()
+end, { desc = "Open Diag in Float" })
+
+vim.keymap.set("n", "<leader>tn", "<cmd>bn<CR>", { desc = "next buffer" })
+
+vim.keymap.set("n", "<leader>tp", "<cmd>bp<CR>", { desc = "previous buffer" })
+
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true, desc = "Move Left (Insert Mode)" })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true, desc = "Move Down (Insert Mode)" })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true, desc = "Move Up (Insert Mode)" })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true, desc = "Move Right (Insert Mode)" })
+
+-- Buffer keymappings
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+
+-- Re-order to previous/next
+map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+
+-- Goto buffer in position...
+map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+
+-- Pin/unpin buffer
+map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
+
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+
+-- Close buffer
+map("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
+map("n", "<A-C>", "<Cmd>BufferCloseAllButCurrent<CR>", opts)
+
+-- Wipeout buffer
+--                 :BufferWipeout
+
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+
+-- Magic buffer-picking mode
+map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
+map("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>", opts)
+
+-- Sort automatically by...
+map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
+map("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>", opts)
+map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
+map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
---
+++ lua/config/options.lua
@@ -0,0 +1,59 @@
+vim.opt.expandtab=true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+--always show signcolumn
+vim.opt.signcolumn = "yes"
+
+vim.opt.cmdheight = 0
+
+vim.g.have_nerd_font = true
+vim.o.showmode = false
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+--split setting corrections
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+--list settings
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 13
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+--move focus for window navigation
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.keymap.set('n', 'bd', '<cmd>bd<CR>', {desc="closes oil with bd rather than :bd"})
+
+vim.keymap.set('n', '<leader>fi', '=G', {desc="Reindent File"})
